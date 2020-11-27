3bf8141a660d241fdb88c8a226f1b4a009bfa849
DAVID T K LEUNG (DEV-ISD-OOCLL/HKG) <LEUNGDA3@OOCL.COM>
2020-11-27 15:50:08 +0800

    [ParkingBoy][David Leung] refactor: clean code

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -25,9 +25,20 @@ class ParkingBoyTest {
     }
 
     @Test
-    public void should_parking_boy_calling_parking_lot_park_function_when_park_the_car_given_parking_lot_has_1_capacity() {
-
-
+    public void should_only_1_car_parked_when_multiple_car_given_parking_lot_has_1_capacity() {
+//        //given
+//        ParkingLot parkingLot = Mockito.mock(ParkingLot.class);
+//        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+//        Car car1 = new Car();
+//        Car car2 = new Car();
+//
+//        //when
+//        Ticket ticket1 = parkingLot.park(car1);
+//        Ticket ticket2 = parkingLot.park(car2);
+//
+//        //then
+//        verify(parkingLot, times(1)).park(car1);
+//        verify(parkingLot, times(0)).park(car2);
     }
 
 
