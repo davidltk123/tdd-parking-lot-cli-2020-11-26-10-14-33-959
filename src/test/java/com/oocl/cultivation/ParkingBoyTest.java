63caeb902f30bb0b9a4c044a6e3d7e99021b0774
DAVID T K LEUNG (DEV-ISD-OOCLL/HKG) <LEUNGDA3@OOCL.COM>
2020-11-27 15:38:17 +0800

    [ParkingBoy][David Leung] test: add parking boy park car with availabe capacity in Mockito

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -3,26 +3,33 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParkingBoyTest {
     @Test
     public void should_parking_boy_calling_parking_lot_park_function_when_park_the_car_given_parking_lot_has_available_capacity() {
 
         //given
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = Mockito.mock(ParkingLot.class);
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         Car car = new Car();
 
         //when
-        Ticket ticket = parkingBoy.park(car);
+        parkingBoy.park(car);
 
         //then
-        assertNotNull(ticket);
+        verify(parkingLot, times(1)).park(car);
+
     }
+
+    @Test
+    public void should_parking_boy_calling_parking_lot_park_function_when_park_the_car_given_parking_lot_has_1_capacity() {
+
+
+    }
+
 
 
 }
