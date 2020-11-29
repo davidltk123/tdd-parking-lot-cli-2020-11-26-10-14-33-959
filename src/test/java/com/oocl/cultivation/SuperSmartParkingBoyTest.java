e0ce0a3e8e14022c8123df3162a582154ddb5e41
davidltk123 <davidltk123@gmail.com>
2020-11-29 15:54:09 +0800

    [Parking Lot S5][David Leung] test: add fetch from parking lot 2 when parked in lot 2 for supersmarkparkingboy

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -67,4 +67,27 @@ public class SuperSmartParkingBoyTest {
         //then
         assertEquals("Not enough position.",notEnoughPositionException.getMessage());
     }
+
+    @Test
+    public void should_fetch_from_second_parking_lot_when_fetch_the_car_given_the_car_parked_in_second_lot() throws NotEnoughPositionException, UnrecognizedParkingTicketException {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(5);
+        ParkingLot parkingLot2 = new ParkingLot(2);
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        parkingLot1.park(new Car());
+        parkingLot1.park(new Car());
+        parkingLot1.park(new Car());
+        parkingLot2.park(new Car());  //parkingLot1 3/5    parkingLot2 1/2
+        Car car = new Car();
+        Ticket ticket = superSmartParkingBoy.park(car);
+
+        //when
+        Car actual = superSmartParkingBoy.fetch(ticket);
+
+        //then
+        assertEquals(car,actual);
+    }
 }
