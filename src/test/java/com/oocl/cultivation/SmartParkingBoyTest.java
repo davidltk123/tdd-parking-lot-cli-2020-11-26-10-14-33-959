b1003218e81b0a7425773161df833e7ed4e0b357
davidltk123 <davidltk123@gmail.com>
2020-11-29 14:12:31 +0800

    [Parking Lot S3][David Leung] test: add fetch car from lot 2 when parked in lot 2

--- src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -60,4 +60,22 @@ public class SmartParkingBoyTest {
         //then
         assertEquals("Not enough position.",notEnoughPositionException.getMessage());
     }
+
+    @Test
+    public void should_fetch_from_second_parking_lot_when_fetch_the_car_given_the_car_parked_in_second_lot() throws NotEnoughPositionException, UnrecognizedParkingTicketException {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(2);
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+        Ticket ticket = smartParkingBoy.park(car);
+        //when
+        Car actual = smartParkingBoy.fetch(ticket);
+
+        //then
+        assertEquals(car,actual);
+    }
 }
