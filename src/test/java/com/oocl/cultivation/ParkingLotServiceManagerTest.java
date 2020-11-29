83483cbfe418c94ab902727ee2eb05a10859aa9b
davidltk123 <davidltk123@gmail.com>
2020-11-29 16:53:13 +0800

    [Parking Lot S6][David Leung] test: add return null when assign a non exist car parking boy to fetch

--- src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
@@ -29,8 +29,7 @@ public class ParkingLotServiceManagerTest {
     public void should_return_ticket_when_specify_parking_boy_to_park_given_a_car_and_parking_boy() throws NotEnoughPositionException {
         //given
         List<ParkingLot> parkingLots = new ArrayList<>();
-        ParkingLot parkingLot = new ParkingLot(1);
-        parkingLots.add(parkingLot);
+        parkingLots.add(new ParkingLot(1));
         ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
 
         ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager(new ArrayList<>());
@@ -69,8 +68,7 @@ public class ParkingLotServiceManagerTest {
     public void should_return_car_when_specify_parking_boy_to_fetch_given_a_valid_ticket_and_parking_boy() throws NotEnoughPositionException, UnrecognizedParkingTicketException {
         //given
         List<ParkingLot> parkingLots = new ArrayList<>();
-        ParkingLot parkingLot = new ParkingLot(1);
-        parkingLots.add(parkingLot);
+        parkingLots.add(new ParkingLot(1));
         ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
 
         ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager(new ArrayList<>());
@@ -83,5 +81,27 @@ public class ParkingLotServiceManagerTest {
 
         //then
         assertEquals(car,actual);
+    }
+
+    @Test
+    public void should_null_car_when_specify_parking_boy_to_fetch_given_not_exist_parking_boy() throws NotEnoughPositionException, UnrecognizedParkingTicketException {
+        //given
+        List<ParkingLot> parkingBoy1ParkingLots = new ArrayList<>();
+        List<ParkingLot> parkingBoy2ParkingLots = new ArrayList<>();
+        parkingBoy1ParkingLots.add(new ParkingLot(1));
+        parkingBoy2ParkingLots.add(new ParkingLot(1));
+        ParkingBoy parkingBoy1 = new ParkingBoy(parkingBoy1ParkingLots);
+        ParkingBoy parkingBoy2 = new ParkingBoy(parkingBoy2ParkingLots);
+
+        ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager(new ArrayList<>());
+        parkingLotServiceManager.addToManagementList(parkingBoy1);
+        Car car = new Car();
+        Ticket ticket = parkingLotServiceManager.specifyParkingBoyToPark(car,parkingBoy1);
+
+        //when
+        Car actual = parkingLotServiceManager.specifyParkingBoyToFetch(ticket,parkingBoy2);
+
+        //then
+        assertNull(actual);
     }
 }
